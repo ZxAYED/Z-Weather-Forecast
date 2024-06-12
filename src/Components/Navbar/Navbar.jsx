2c2720d27a0e9efd8b72018750504681c6239c64
zxayed <zzayedghost@gmail.com>
2024-06-12 10:36:06 +0600

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+
+
+
+  
+
+const Navbar = () => {
+
+return(
+   <div className='bg-white rounded-lg '>
+    <section>
+      <h1 className='text-3xl font-bold text-center'>Search option</h1>
+    </section>
+
+   </div>
+)
+}
+
+
+
+export default Navbar;
